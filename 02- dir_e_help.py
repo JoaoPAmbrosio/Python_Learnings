d7cd096c915fce16c9dfe90d4e1769c06dd47083
Joao Pedro Ambrosio <joaopeambrosio@gmail.com>
2023-05-09 16:51:03 -0300

    Practicing Dir and Help

---
+++ 02- dir_e_help.py
@@ -0,0 +1,15 @@
+"""
+Utilitarios Python para te auxiliar na promamação.
+
+dir -> Apresenta todos so atributos e funções/métorods disponíveis para determinado tipo de dado ou variável
+
+dir(tipo de dado/variável)
+
+help -> Apresenta a dcumentação/como utilizar os atributos/propriedades e funções/métodos disponíveis
+para determinado tipo de dado ou variável.
+
+help(tipo de dado/variável.propriedade)
+
+"""
+
+print(dir("Joao"))
