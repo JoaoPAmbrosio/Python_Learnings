2d9e5076958ef75bdc5e98b70f571400e1b305e8
Joao Pedro Ambrosio <joaopeambrosio@gmail.com>
2023-05-30 21:50:35 -0300

    Update ranges

--- 12- entendendo_ranges.py
+++ 12- entendendo_ranges.py
@@ -12,7 +12,6 @@ Formas gerais:
 # Forma 01
 range(valor_de_parada)
 OBS: valor_de_parada não inclusive (início padrão 0, e passo de 1 em 1)
-
 # Exemplo Forma 01
 for num in range(11):
     print(num)
@@ -20,7 +19,6 @@ for num in range(11):
 # Forma 02
 range(valor_de_parada, valor_de_parada)
 OBS: valor_de_parada não inclusive (início especificado pelo usuário, e passo de 1 em 1)
-
 # Exemplo Forma 02
 for num in range(1, 11):
     print(num)
@@ -28,7 +26,6 @@ for num in range(1, 11):
 # Forma 03
 range(valor_de_parada, valor_de_parada, passo)
 OBS: valor_de_parada não inclusive (início especificado pelo usuário, e passo especificado pelo usuario)
-
 # Exemplo Forma 03
 for num in range(1, 11, 2):
     print(num)
@@ -36,9 +33,7 @@ for num in range(1, 11, 2):
 # Forma 04 (inversa)
 range(valor_final, valor_de_parada, passo)
 OBS: valor_de_parada não inclusive (início especificado pelo usuário, e decremento especificado pelo usuario)
-
 # Exemplo Forma 04
 for num in range(10, 0, -1):
     print(num)
 """
-
