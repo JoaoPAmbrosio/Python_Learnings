74606c3a6b73b72a4929bdd22bff44dd8f7ba1d4
Joao Pedro Ambrosio <joaopeambrosio@gmail.com>
2023-06-02 09:59:00 -0300

    Breaks learnings

---
+++ 14-breaks.py
@@ -0,0 +1,29 @@
+"""
+Saindo de loops com break
+
+Funciona da mesma forma que nas linguagens C ou Java
+
+Utilizamos o break para sair de loops de maneira projetada.
+
+# Exemplo 01
+
+for numero in range(1, 11):
+    if numero == 6:
+        break
+    else:
+        print(numero)
+print("sai do loop")
+
+"""
+
+
+# Exemplo 02
+vezes = 0
+while True:
+    comando = input("Digite 'sair' para sair: ")
+    vezes += 1
+    if comando == "sair":
+        print(f'voce demorou {vezes} vezes para sair do loop!')
+        break
+    else:
+        print("voce ainda está no loop ")
