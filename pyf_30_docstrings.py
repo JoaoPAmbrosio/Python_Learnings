6bfefa1a31b5f8807d94285924e91bb10b11663a
Joao Pedro Ambrosio <joaopeambrosio@gmail.com>
2023-06-19 10:47:28 -0300

    Map learnings

---
+++ pyf_30_docstrings.py
@@ -0,0 +1,17 @@
+"""
+Documentando funções Docstrings,
+
+"""
+# Exemplos
+def diz_oi():
+    """Uma função simples que retorna a string 'Oi!'"""
+    return 'Oi! '
+print(diz_oi())
+
+def exponencial(numero, potencia=2):
+    """
+    Função que retorna por padrão o quadrado de 'numero' ou 'numero' á 'potencia' informada.
+    :param numero: Número que desejamos gerar o exponencial.
+    :param potencia: Potência que queremos gerar o exponencial. Por padrão é 2.
+    :return: Retorna o Exponencial de 'numero' por 'potencia'.
+    """
