df8931f9bc4462ef76372d6103690608d9adf24c
Joao Pedro Ambrosio <joaopeambrosio@gmail.com>
2023-06-23 10:06:37 -0300

    pep8 dictionary comprehension

--- pyf_36_dictionary_comprehension.py
+++ pyf_36_dictionary_comprehension.py
@@ -14,9 +14,7 @@ dicionario = {'a': 1, 'b': 2, 'c': 3, 'd':4}
 {chave:valor for valor in iterável}
 
 # Exemplo 01
-
 numeros = {'a': 1, 'b': 2, 'c': 3, 'd':4, 'e':5}
-
 quadrado = {chave: valor ** 2 for chave, valor in numeros.items()}
 print(quadrado) # R: {'a': 1, 'b': 4, 'c': 9, 'd': 16, 'e': 25}
 
@@ -30,13 +28,8 @@ print(len(chaves))
 
 mistura = {chaves[i]: valores[i] for i in range(0, len(chaves))}
 print(mistura)
-
 """
-# Exemplo com l[ogica condicional
-
+# Exemplo com lógica condicional
 numeros = [1, 2, 3, 4, 5]
-
 res = {num:('par' if num % 2 == 0 else 'impar') for num in numeros}
 print(res)
-
-
