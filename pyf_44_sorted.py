3e01f2c75c11108e38a38968df2645edd215b982
Joao Pedro Ambrosio <joaopeambrosio@gmail.com>
2023-07-03 11:46:55 -0300

    pep8 sorted

--- pyf_44_sorted.py
+++ pyf_44_sorted.py
@@ -4,21 +4,15 @@ Sorted - integrated function
 OBS: Não confundi com função sort() de Listas. O sort() só funciona em Listas
 
 Pode-se utilizar o sorted() com qualquer iterável.
-
 Como o próprio nome diz, sorted() serve para ordenar.
 OBS: O sorted sempre retorna uma lista com os elementos iteráveis ordenados
 
 # Exemplo
-
 numeros = (6, 1, 8, 2)
-
 print(sorted(numeros)) # Ordenar do menor para o maior, retornando em lista. R: [1, 2, 6, 8]
 print(numeros) # R: (6, 1, 8, 2)
 
-numeros = (6, 1, 8, 2)
-
 # Adicionando parâmetros ao sorted()
-
 print(sorted(numeros, reverse=True)) # Ordena do maior para o menor, em lista R:[8, 6, 2, 1]
 
 # Pode-se utilizar o sorted() para coisas mais complexas
@@ -37,10 +31,8 @@ print(sorted(usuarios, key=lambda u: u["username"], reverse=True))
 
 # Ordenar pelo numero de tweets. Ordem decrescente com o reverse
 print(sorted(usuarios, key=lambda u: len(u["tweets"]), reverse=True))
-
 """
 # Exemplo
-
 musicas = [
     {"titulo": "Thunderstruck", "tocou": 3},
     {"titulo": "Dead Skin Mask", "tocou": 2},
@@ -49,5 +41,3 @@ musicas = [
 ]
 print(musicas[0]["tocou"])
 print(sorted(musicas, key=lambda m: m["tocou"], reverse=True))
-
-
